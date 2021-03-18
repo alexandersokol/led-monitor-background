ed72af9961c705e5f9ede5986bdd4d371a668758
Alexander Sokol <sokol.a@dbbest.com>
2021-03-18 19:27:08 +0200

    freezed version

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <OneButton.h>
 
 #include "const.h"
+#include "Effect.h"
+#include "LinearEffect.h"
+#include "RadialEffect.h"
 
 CRGB leds[LED_COUNT];
 
@@ -12,17 +15,33 @@ OneButton buttonPrevious(PIN_LEFT, true, true);
 OneButton buttonNext(PIN_RIGHT, true, true);
 OneButton buttonOk(PIN_OK, true, true);
 
+CRGB::HTMLColorCode colors[] = {CRGB::White, CRGB::OldLace, CRGB::LightYellow, CRGB::White, CRGB::Wheat, CRGB::WhiteSmoke, CRGB::AliceBlue, CRGB::OldLace, CRGB::Olive, CRGB::Azure, CRGB::Amethyst, CRGB::Gainsboro, CRGB::DarkGoldenrod, CRGB::Gold, CRGB::Red, CRGB::OrangeRed, CRGB::MediumPurple, CRGB::SeaGreen, CRGB::MediumVioletRed, CRGB::Orchid, CRGB::HotPink, CRGB::Honeydew, CRGB::FairyLight, CRGB::Fuchsia, CRGB::SandyBrown, CRGB::SkyBlue, CRGB::Chocolate, CRGB::Cornsilk, CRGB::PaleVioletRed, CRGB::Bisque, CRGB::BurlyWood, CRGB::MidnightBlue, CRGB::FireBrick, CRGB::GreenYellow, CRGB::Khaki, CRGB::LawnGreen, CRGB::LemonChiffon, CRGB::LightSalmon, CRGB::RoyalBlue, CRGB::Thistle, CRGB::Tan, CRGB::Teal, CRGB::LightGoldenrodYellow, CRGB::Indigo, CRGB::Ivory, CRGB::OliveDrab, CRGB::Plum};
 int position = 0;
-CRGB::HTMLColorCode colors[] = {CRGB::OldLace, CRGB::LightYellow, CRGB::White, CRGB::Wheat, CRGB::WhiteSmoke, CRGB::AliceBlue, CRGB::OldLace, CRGB::Olive, CRGB::Azure, CRGB::Amethyst, CRGB::Gainsboro, CRGB::DarkGoldenrod, CRGB::Gold, CRGB::Red, CRGB::OrangeRed, CRGB::MediumPurple, CRGB::SeaGreen, CRGB::MediumVioletRed, CRGB::Orchid, CRGB::HotPink, CRGB::Honeydew, CRGB::FairyLight, CRGB::Fuchsia, CRGB::SandyBrown, CRGB::SkyBlue, CRGB::Chocolate, CRGB::Cornsilk, CRGB::PaleVioletRed, CRGB::Bisque, CRGB::BurlyWood, CRGB::MidnightBlue, CRGB::FireBrick, CRGB::GreenYellow, CRGB::Khaki, CRGB::LawnGreen, CRGB::LemonChiffon, CRGB::LightSalmon, CRGB::RoyalBlue, CRGB::Thistle, CRGB::Tan, CRGB::Teal, CRGB::LightGoldenrodYellow, CRGB::Indigo, CRGB::Ivory, CRGB::OliveDrab, CRGB::Plum};
+int brightness = LED_BRIGHTNESS;
+
+effects::Effect *effect;
+effects::LinearEffect linear;
+effects::RadialEffect radial;
+int pos = 0;
 
 void increaseBrightness()
 {
-  Serial.println("Increase Brightness");
+  brightness = brightness + 5;
+  Serial.println(brightness);
+  if (brightness > LED_MAX_BRIGHTNESS)
+  {
+    brightness = LED_MAX_BRIGHTNESS;
+  }
+  Serial.println(brightness);
 }
 
 void decreaseBrightness()
 {
-  Serial.println("Decrease Brightness");
+  brightness = brightness - 5;
+  if (brightness < LED_MIN_BRIGHTNESS)
+  {
+    brightness = LED_MIN_BRIGHTNESS;
+  }
 }
 
 void previousMode()
@@ -49,18 +68,23 @@ void nextMode()
 
 void okFunc()
 {
-  Serial.print("Array Length: ");
-  Serial.println(lengthOf(colors), DEC);
+  if(pos == 0){
+    pos++;
+    effect = &radial;
+  } else {
+    pos = 0;
+    effect = &linear;
+  }
 }
 
 void setup()
 {
   Serial.begin(9600);
 
-  delay(2000);
+  delay(1000);
 
   FastLED.addLeds<CHIPSET, PIN_LED, COLOR_ORDER>(leds, LED_COUNT).setCorrection(TypicalLEDStrip);
-  FastLED.setBrightness(LED_BRIGHTNESS);
+  FastLED.setBrightness(brightness);
 
   buttonUp.attachClick(increaseBrightness);
   buttonDown.attachClick(decreaseBrightness);
@@ -87,6 +111,7 @@ void loop()
     leds[i] = colors[position];
   }
 
+  FastLED.setBrightness(brightness);
   FastLED.show(); // display this frame
   FastLED.delay(1000 / FRAMES_PER_SECOND);
 }
